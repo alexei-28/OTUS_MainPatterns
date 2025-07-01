5f2aa2e5a2bae892abeb4b4c5d8c1a632d4f7b63
alexei <a_subscriber@mail.ru>
2025-07-01 22:15:04 +0300

    init .gitignore

--- app/src/main/java/ru/otus/main_patterns/Main.java
+++ app/src/main/java/ru/otus/main_patterns/Main.java
@@ -1,4 +1,8 @@
 package ru.otus.main_patterns;
 
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Java version: " + System.getProperty("java.version"));
+    }
 }
---
+++ app/src/test/java/ru/otus/main_patterns/AppTest.java
@@ -0,0 +1,10 @@
+package ru.otus.main_patterns;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test public void appHasAGreeting() {
+       assertTrue(true);
+    }
+}
